d0451d143a76193a972fbcceb8872b649ba88ac1
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2017-04-18 14:31:09 -0700

    :wrench: Added prototype build

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable global-require */
-/* eslint-disable import/no-extraneous-dependencies */
-
-module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('postcss-cssnext'),
-  ],
-};
--- src/utils/Theme.js
+++
@@ -1,9 +0,0 @@
-import { getMuiTheme } from 'material-ui/styles';
-import { purple500, orangeA200 } from 'material-ui/styles/colors';
-
-export default getMuiTheme({
-  palette: {
-    primary1Color: purple500,
-    accent1Color: orangeA200,
-  },
-});
