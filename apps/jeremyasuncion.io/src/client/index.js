4e9e942b598514f69457df4f61358daf9cd7f861
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2017-07-03 00:42:48 -0700

    :wrench: Refactored client entry

--- apps/jeremyasuncion.io/src/client/index.js
+++ apps/jeremyasuncion.io/src/client/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
-
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
+import { BrowserRouter } from 'react-router-dom';
+import { MuiThemeProvider } from 'material-ui';
 
-import AppRouter from './app-router';
+import App from '../shared/app';
+import { createStyleManager } from '../shared/theme';
 
 function renderApp() {
+  const { styleManager, theme } = createStyleManager();
   render((
     <AppContainer>
-      <AppRouter />
+      <BrowserRouter>
+        <MuiThemeProvider styleManager={styleManager} theme={theme}>
+          <App />
+        </MuiThemeProvider>
+      </BrowserRouter>
     </AppContainer>
   ));
 }
