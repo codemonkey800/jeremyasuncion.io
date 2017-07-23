a05aa6ee1585b56399639f46ed58957d61b99eca
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2017-07-22 18:39:41 -0700

    :wrench: Starting project from scratch, thinking about using Vue.js

--- apps/jeremyasuncion.io/scripts/common.js
+++
@@ -1,6 +0,0 @@
-import path from 'path';
-
-export const dev = process.env.NODE_ENV !== 'production';
-
-export const resolve = path.resolve.bind(null, __dirname, '..');
-
--- apps/jeremyasuncion.io/scripts/webpack/index.js
+++
@@ -1,4 +0,0 @@
-export client from './client.config';
-export common from './common.config';
-export server from './server.config';
-
