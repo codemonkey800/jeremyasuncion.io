074c9068be0bfe31f27748e4e8f6ac0d59e7d988
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2017-09-07 02:54:20 -0700

    :tada: Removed YouTube playlist button, changed "Google Intern" to "SJSU Student"

--- apps/jeremyasuncion.io/public/index.js
+++ apps/jeremyasuncion.io/public/index.js
@@ -7,7 +7,7 @@
   const additionalStrings = [
     'Software Engineer',
     'Web Developer',
-    'Google Intern',
+    'SJSU Student',
   ];
   const lastLoadedTime = parseInt(localStorage.getItem('lastLoaded'), 10) || 0;
   const lastLoaded = new Date(lastLoadedTime);
